910103f677aa550ddaf1a334aae763920da41730
rik Hoevenaars <588736@student.fontys.nl>
2026-05-21 15:01:13 +0200

    Booqr project structure

--- Booqr.Logic/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Booqr.Logic
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Booqr.Web/Pages/Reservations/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Booqr.Web.Pages.Reservations
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
