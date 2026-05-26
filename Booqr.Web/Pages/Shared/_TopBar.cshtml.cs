64f4e4d4cc0b8d41bd60a89c6492419cb2de76d9
rik Hoevenaars <588736@student.fontys.nl>
2026-05-26 12:28:45 +0200

    Homepage-Interface

---
+++ Booqr.Web/Pages/Shared/_TopBar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Booqr.Web.Pages.Shared
+{
+    public class _TopBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
